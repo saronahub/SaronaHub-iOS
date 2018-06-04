6af709d4febbbc7663384dc90c65f5a5872dc88d
asafall <asafall21@gmail.com>
2018-06-05 02:00:18 +0300

    Added properties to EventTableViewController
    in EventTableViewController.swift:
    added UIViews outlets and image properties.
    added event property.
    
    in Main.storyboard:
    connected the outlets to the class.

--- SaronaHub/EventTableViewController.swift
+++ SaronaHub/EventTableViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class EventTableViewController: UITableViewController {
 
+    var event: Event?
+    
+    var image: UIImage?
+    
+    @IBOutlet weak var eventImageView: UIImageView!
+    
+    @IBOutlet weak var eventNameLabel: UILabel!
+    
+    @IBOutlet weak var eventHostLabel: UILabel!
+    
+    @IBOutlet weak var eventDateLabel: UILabel!
+    
+    @IBOutlet weak var eventDescriptionTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
