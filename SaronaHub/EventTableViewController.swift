3e0ffbe5b6948bc4005616eda1579be94aab9680
asafall <asafall21@gmail.com>
2018-06-05 02:38:41 +0300

    presenting event data in EventTableViewController

--- SaronaHub/EventTableViewController.swift
+++ SaronaHub/EventTableViewController.swift
@@ -26,6 +26,9 @@ class EventTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let event = self.event {
+            self.initiate(from: event)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,4 +36,52 @@ class EventTableViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func initiate(from event: Event) {
+        self.eventNameLabel.text = event.name
+        if let eventAuthor = event.eventAuthor {
+            self.eventHostLabel.text = eventAuthor.FullName
+        }
+        
+        var day, date, hours: String?
+        var startTime, endTime: String?
+        
+        if let startDate = event.startDate {
+            let formatter = DateFormatter()
+            formatter.locale = Locale(identifier: "he_IL")
+            day = formatter.weekdaySymbols[Calendar.current.component(.weekday, from: startDate) - 1]
+            date = "\(Calendar.current.component(.day, from: startDate)) ב\(formatter.monthSymbols[Calendar.current.component(.month, from: startDate) - 1])"
+            let components = Calendar.current.dateComponents([.hour, .minute], from: startDate)
+            if let hour = components.hour, let minute = components.minute {
+                startTime = "\(hour < 10 ? "0\(hour)" : "\(hour)"):\(minute < 10 ? "0\(minute)" : "\(minute)")"
+            }
+        }
+        if let endDate = event.endDate {
+            let components = Calendar.current.dateComponents([.hour, .minute], from: endDate)
+            if let hour = components.hour, let minute = components.minute {
+                endTime = "\(hour < 10 ? "0\(hour)" : "\(hour)"):\(minute < 10 ? "0\(minute)" : "\(minute)")"
+            }
+        }
+        
+        if let startTime = startTime, let endTime = endTime {
+            hours = "\(startTime) - \(endTime)"
+        } else if let startTime = startTime {
+            hours = startTime
+        } else if let endTime = endTime {
+            hours = endTime
+        }
+        
+        var dateText: String = ""
+        if let day = day {
+            dateText += "\(day)"
+        }
+        if let hours = hours {
+            dateText += " \(hours)"
+        }
+        if let date = date {
+            dateText += ", \(date)"
+        }
+        self.eventDateLabel.text = dateText
+        self.eventDescriptionTextView.text = event.description
+        self.eventImageView.image = self.image
+    }
 }
